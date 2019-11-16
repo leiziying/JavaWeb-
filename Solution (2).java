38487c679e98a0d98b7d7d4cfbf9f36f580e2360
leiziying <3032483701@qq.com>
2019-11-16 22:58:02 +0800

    牛客网每日一题11-16

---
+++ Solution (2).java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[]  multiply(int[] A){
+        if(A==null&&A.length==0){
+            return A;
+        }
+        int[] B=new int[A.length];
+        for (int i = 0; i <A.length ; i++) {
+            int sum=1;
+            for (int j = 0; j <A.length ; j++) {
+                if(i!=j){
+                    sum*=A[j];
+                }
+            }
+            B[i]=sum;
+        }
+        return B;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

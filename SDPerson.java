558cd35c5b3f256d9bafe4e34e685d638fbea4da
leiziying <3032483701@qq.com>
2019-12-03 22:47:59 +0800

    提交代码

---
+++ SDPerson.java
@@ -0,0 +1,10 @@
+public class SDPerson {
+        public static void main(String[] args) {
+            Thread t1=new Thread();
+            Thread  t2=new Thread();
+            t1.start();
+            t2.start();
+        }
+    }
+
+

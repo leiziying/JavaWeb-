399e3246fbf05a512abb39477dedd0e18740aacb
leiziying <3032483701@qq.com>
2019-12-26 12:54:18 +0800

    基于udp协议实现客户端

---
+++ Client1226.java
@@ -0,0 +1,27 @@
+package tcp.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.*;
+
+public class Client1226 {
+    public static void main(String[] args) throws IOException {
+        Socket tcpClientSocket=new Socket();
+        byte[] ipv4={127,0,0,1};
+        InetAddress serverAddress=InetAddress.getByAddress(ipv4);
+        SocketAddress serverSocketAddress= new InetSocketAddress(serverAddress,8080);
+        tcpClientSocket.connect(serverSocketAddress);
+       //通过字节流直接写入请求
+        OutputStream os=tcpClientSocket.getOutputStream();
+        os.write("我是好人\r\n".getBytes("UTF-8"));
+        //通过字节流，直接读取数据
+        InputStream is=tcpClientSocket.getInputStream();
+        byte[]  buffer=new byte[1024];
+        int len=is.read(buffer);
+        String response=new String (buffer,0,len,"UTF-8");
+        System.out.println(response);
+        tcpClientSocket.close();
+    }
+}

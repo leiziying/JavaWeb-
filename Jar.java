b06b0d0d0d3bb9e267abae28c05b039e6a5a62f0
leiziying <3032483701@qq.com>
2020-01-07 23:00:32 +0800

    java---jar(2020-01-07)

---
+++ Jar.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Jar {
+    //jar  -cvf  Main.jar A.class B.class
+    public static void main(String[] args) throws FileNotFoundException {
+      args[0]="-cvf";
+      args[1]="Main.jar";
+      args[2]="A.class";
+      args[3]="B.class";
+      //1.String解析  -cvf
+        OutputStream os=new FileOutputStream(args[1]);
+        InputStream is1=new FileInputStream(args[2]);
+        InputStream is2=new FileInputStream(args[3]);
+    }
+}
